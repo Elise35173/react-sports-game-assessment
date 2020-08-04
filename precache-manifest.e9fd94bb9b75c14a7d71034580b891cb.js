e8e5f2760c5d01da84e282c7089ce09e07cf8444
Elise Cossairt <elise_cossairt@hotmail.comgit config --global user.name Eliserunclear--globalgit config user.email elise_cossairt@hotmail.com>
2020-08-04 10:47:08 -0500

    Updates

---
+++ precache-manifest.e9fd94bb9b75c14a7d71034580b891cb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584ee5d3c450bf73dfe0301817e88c6d",
+    "url": "/react-sports-game-assessment/index.html"
+  },
+  {
+    "revision": "ea01990ffb53b342ba88",
+    "url": "/react-sports-game-assessment/static/css/main.3bde31c1.chunk.css"
+  },
+  {
+    "revision": "ebc7dfc70598f0156828",
+    "url": "/react-sports-game-assessment/static/js/2.7005453d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-sports-game-assessment/static/js/2.7005453d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea01990ffb53b342ba88",
+    "url": "/react-sports-game-assessment/static/js/main.677e44d4.chunk.js"
+  },
+  {
+    "revision": "869bfc85debc3fcd1f5b",
+    "url": "/react-sports-game-assessment/static/js/runtime-main.4bd516d6.js"
+  },
+  {
+    "revision": "edb1b73edaab9e9eae88c23b1a24308b",
+    "url": "/react-sports-game-assessment/static/media/Back+Board.edb1b73e.wav"
+  },
+  {
+    "revision": "c97879711c304a1ef5d92b49cb02f6c2",
+    "url": "/react-sports-game-assessment/static/media/Swish.c9787971.wav"
+  },
+  {
+    "revision": "fcecaee6bf630c0668625a247da24a4e",
+    "url": "/react-sports-game-assessment/static/media/shotclockhorn.fcecaee6.wav"
+  }
+]);
